8a321f661ea81cac1ecc5365e850d1c0aac2f697
Sajgon27 <szymon.mudrak@gmail.com>
2025-05-28 11:45:56 +0200

    Change loader.js

--- loader.js
+++ loader.js
@@ -1,8 +1,10 @@
-export default function imgixLoader = ({ src, width, quality }) => {
-  const url = new URL(`https://smd-web-314690922.imgix.net${src}`)
-  const params = url.searchParams
-  params.set("auto", params.getAll("auto").join(",") || "format")
-  params.set("fit", params.get("fit") || "max")
-  params.set("w", params.get("w") || width.toString())
-  return url.href
-}
+const imgixLoader = ({ src, width, quality }) => {
+  const url = new URL(`https://smd-web-314690922.imgix.net${src}`);
+  const params = url.searchParams;
+  params.set("auto", params.getAll("auto").join(",") || "format");
+  params.set("fit", params.get("fit") || "max");
+  params.set("w", params.get("w") || width.toString());
+  return url.href;
+};
+
+export default imgixLoader;
